a0747c92bbfad5efdd5ccb1fe81667f1dbaa1e9b
ERM87M <maziratira@gmail.com>
2024-01-12 23:13:28 +0300

    4.1. Аргументы [#295521]

---
+++ src/main/java/ru/job4j/ArgumentsMethod.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+public class ArgumentsMethod {
+
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", age = " + age);
+    }
+
+    public static void main(String[] args) {
+        String name = "Job4j";
+        int age = 6;
+        ArgumentsMethod.hello(name, age);
+        ArgumentsMethod.hello(name, age);
+        ArgumentsMethod.hello(name, age);
+        ArgumentsMethod.hello(name, age);
+
+    }
+}
