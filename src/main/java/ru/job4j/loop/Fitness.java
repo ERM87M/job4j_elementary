cd5115c7c0f1d43f9eec5c8e9cadd4be5c59c985
ERM87M <maziratira@gmail.com>
2024-03-27 16:07:20 +0300

    5.4. Протеиновая диета (цикл while) [#156313]

---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan <= nik) {
+            ivan *= 3;
+            nik *= 2;
+            month += 1;
+        }
+        return month;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Fitness.calc(90, 60));
+    }
+}
