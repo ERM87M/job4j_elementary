8355feb40fd0a4a66a37f8c48098656a70e9439f
ERM87M <maziratira@gmail.com>
2024-04-20 14:24:27 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,4 +1,9 @@
 package ru.job4j.array;
+/*
+  6.0.1. Объявление массива. [#3613 #492235]
+  6.0.2. Размер массива [#333582 #492238]
+  6.0.3. Заполнение массива. [#226300]
+ */
 
 public class ArrayDefinition {
     public static void main(String[] args) {
@@ -8,5 +13,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Roman";
+        names[1] = "Alex";
+        names[2] = "Andrey";
+        names[3] = "Nikolya";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
