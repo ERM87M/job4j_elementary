af23165c4460cf165809d601c47f11134f2565e9
ERM87M <maziratira@gmail.com>
2024-04-20 13:37:17 +0300

    6.0.1. Объявление массива. [#3613]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+        System.out.println(ages.length);
+        System.out.println(surnames.length);
+        System.out.println(prices.length);
+    }
+}
