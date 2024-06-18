5d95bd8619d11d01a2c005b51a3a2ed9849ff570
ERM87M <maziratira@gmail.com>
2024-06-18 12:43:14 +0300

    6.4.0. Слово начинается с ... [#41585]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] prefix) {
+        boolean result = true;
+        for (int i = 0; i < prefix.length; i++) {
+            if (prefix[i] != word[i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
