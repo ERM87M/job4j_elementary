0c37eefb11ba94ea7c5ce4dd6f4c492da04ee3b7
ERM87M <maziratira@gmail.com>
2025-01-02 00:24:07 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/calculator/Calculate.java
+++ src/main/java/ru/job4j/calculator/Calculate.java
@@ -1,6 +1,6 @@
 package ru.job4j.calculator;
 
-public class Calculator {
+public class Calculate {
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
--- src/main/java/ru/job4j/calculator/MathCalculate.java
+++ src/main/java/ru/job4j/calculator/MathCalculate.java
@@ -2,7 +2,7 @@ package ru.job4j.calculator;
 
 import static ru.job4j.math.MathFunction.*;
 
-public class MathCalculator {
+public class MathCalculate {
     public static double sumAndMultiply(double first, double second) {
         return sum(first, second)
                 + multiply(first, second);
