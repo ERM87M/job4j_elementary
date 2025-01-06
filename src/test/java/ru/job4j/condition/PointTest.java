9dcd76ddba4e734d29310c4cd824648aec3483c1
ERM87M <maziratira@gmail.com>
2025-01-06 23:43:47 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -3,6 +3,7 @@ package ru.job4j.condition;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PointTest {
 
@@ -31,5 +32,37 @@ class PointTest {
         double expected = 6.32;
         double output = x.distance(y);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    public void whenDistance3dIs0() {
+        Point p1 = new Point(0, 0, 0);
+        Point p2 = new Point(0, 0, 0);
+        double result = p1.distance3d(p2);
+        assertEquals(0, result, 0.01);
+    }
+
+    @Test
+    public void whenDistance3dIs2() {
+        Point p1 = new Point(0, 0, 0);
+        Point p2 = new Point(0, 2, 0);
+        double result = p1.distance3d(p2);
+        assertEquals(2, result, 0.01);
+    }
+
+    @Test
+    public void whenDistance3dIsSqrt12() {
+        Point p1 = new Point(0, 0, 0);
+        Point p2 = new Point(2, 2, 2);
+        double result = p1.distance3d(p2);
+        assertEquals(Math.sqrt(12), result, 0.01);
+    }
+
+    @Test
+    public void whenDistance3dIsDiagonal() {
+        Point p1 = new Point(1, 2, 3);
+        Point p2 = new Point(4, 6, 8);
+        double result = p1.distance3d(p2);
+        assertEquals(Math.sqrt(3 * 3 + 4 * 4 + 5 * 5), result, 0.01);
     }
 }
